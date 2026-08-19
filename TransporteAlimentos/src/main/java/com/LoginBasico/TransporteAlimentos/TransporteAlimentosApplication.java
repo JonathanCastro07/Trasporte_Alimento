e994258245d5380e5311d673c7674a0f3738ba0a
jonathan <jmanjarres238@gmail.com>
2026-08-18 19:42:38 -0500

    primer commit

---
+++ TransporteAlimentos/src/main/java/com/LoginBasico/TransporteAlimentos/TransporteAlimentosApplication.java
@@ -0,0 +1,13 @@
+package com.LoginBasico.TransporteAlimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteAlimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransporteAlimentosApplication.class, args);
+	}
+
+}
---
+++ TransporteAlimentos/src/test/java/com/LoginBasico/TransporteAlimentos/TransporteAlimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LoginBasico.TransporteAlimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporteAlimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
